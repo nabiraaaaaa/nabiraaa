467a43e0c96383d411ca74efde3e3ffe52c146e8
nabiraaaaaa <nabiraagiztabia@gmail.com>
2025-05-20 08:17:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🫧 Informatika")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Belajar informatika bersama pak hendri."
 )
+st.image("IMG_4659.jpeg", width=200)
